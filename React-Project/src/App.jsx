23ca5b32a5e58abc620fe10fb971815e17d014b8
AtsushiTsujita <mozu.077@gmail.com>
2026-03-04 09:39:55 +0900

    App.jsxにスレッド一覧表示機能を追加

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [threads, setThreads] = useState([]);
+
+  useEffect(() => {
+    const getThreads = async () => {
+      const url = 'https://railway.bulletinboard.techtrain.dev/threads';
+      const response = await fetch(url);
+      const result = await response.json();
+      setThreads(result);
+    };
+
+    getThreads();
+
+  }, []);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>新着スレッド</h1>
+      <div className='threads'>
+        {threads.map((thread) => (
+          <p key={thread.id} className='title'>{thread.title}</p>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
-}
 
+}
 export default App
